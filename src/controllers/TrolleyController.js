243c9578286210fae1ce9e23a03f4e653bdea3d0
Luan Severo <luansevero@icloud.com>
2022-07-06 20:56:13 -0300

    feat: TrolleyController( GET -> get all products in the costumer trolley)

--- src/controllers/TrolleyController.js
+++ src/controllers/TrolleyController.js
@@ -6,11 +6,23 @@ const trolley = {
         const { costumer } = res.locals;
         const product = req.body;
         try{
-            await db.collection('trolleyProducts').insertOne({...product, userId: costumer._id})
+            await db.collection('trolley-products').insertOne({...product, userId: costumer._id})
             res.sendStatus(200);
         }catch(error){
             console.log("[Error] - postProduct Trolley Controller");
             res.sendStatus(500);
         }
-    }
+    },
+    getProducts: async function(req,res){
+        const { costumer } = res.locals;
+        try{
+            const costumerTrolley = await db.collection('trolley-products').find({userId: costumer._id},{projection:{_id:0, userId:0}}).toArray();
+            if(!costumerTrolley){return res.sendStatus(401)};
+
+            res.send(costumerTrolley);
+        }catch(error){
+            console.log("[Error] - getProducts Trolley Controller");
+            res.sendStatus(500);
+        }
+    },
 }
