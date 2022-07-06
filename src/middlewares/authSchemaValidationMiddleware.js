4e97318d5be10f0b00357469214b34bd14bd47a1
Luan Severo <luansevero@icloud.com>
2022-07-06 13:36:40 -0300

    fix: Auth schema validation middleware (SignIn and SignUp)

--- src/middlewares/authSchemaValidationMiddleware.js
+++ src/middlewares/authSchemaValidationMiddleware.js
@@ -1,18 +1,17 @@
 import { signInSchema, signUpSchema } from "../schemas/authSchema";
 
-const authSchema = {
-    signInValidation: function(req,res,next){
+function signInValidation(req,res,next){
         const costumer = req.body;
         const validation = signInSchema.validate(costumer);
         if(validation.error){return res.sendStatus(400)};
         next();
-    },
-    singUpValidation: function(req,res,next){
+}
+
+function singUpValidation(req,res,next){
         const { email, password } = req.body;
         const validation = signUpSchema.validate({email:email, password:password});
         if(validation.error){return res.sendStatus(400)};
         next();
-    }
 }
 
-export default authSchema
+export { signInValidation, singUpValidation }
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { signUp, signIn } from "../controllers/authController.js";
+import authSchema from "../middlewares/authSchemaValidationMiddleware.js";
+
 
 const authRoute = Router();
 
-authRoute.post("/signIn", signIn);
-authRoute.post("/signUp", signUp);
+authRoute.post("/signIn",authSchema.signInValidation, signIn);
+authRoute.post("/signUp",authSchema.singUpValidation, signUp);
 
 export default authRoute;
