f8e976866b21a3dc9b924356fe02af0123d67fcb
Luan Severo <luansevero@icloud.com>
2022-07-06 13:39:12 -0300

    fix: Auth Route Imports (SignIn and SignUp)

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { signUp, signIn } from "../controllers/authController.js";
-import authSchema from "../middlewares/authSchemaValidationMiddleware.js";
+import { signInValidation, signUpValidation } from "../middlewares/authSchemaValidationMiddleware.js";
+
 
 
 
 const authRoute = Router();
 
-authRoute.post("/signIn",authSchema.signInValidation, signIn);
-authRoute.post("/signUp",authSchema.singUpValidation, signUp);
+authRoute.post("/signIn",signInValidation, signIn);
+authRoute.post("/signUp",signUpValidation, signUp);
 
 export default authRoute;
