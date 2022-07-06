d6727cfd3b336ab91dbfe25bd5c881720511449d
Luan Severo <luansevero@icloud.com>
2022-07-06 12:31:27 -0300

    feat: main route

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import express, { json } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-
 dotenv.config();
+
+import router from './routes/routes';
 
 const server = express();
 server.use(cors());
 server.use(json());
+server.use(router);
 
 
 const PORT = process.env.PORT
