c0debb212175cb68ef25642d4a7a96ac56d52aba
Luan Severo <luansevero@icloud.com>
2022-07-07 14:38:56 -0300

    feat:Global and Reset css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import ReactDOM from 'react-dom';
+import './assets/css/reset.css'
+import './assets/css/GlobalStyle.js'
 import App from './App';
 
 ReactDOM.render(<App />, document.querySelector('.root'));
