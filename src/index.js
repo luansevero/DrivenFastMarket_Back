f9123e0b5c9eff1f3d847ea70a8053a6d7557019
Luan Severo <luansevero@icloud.com>
2022-07-07 14:42:15 -0300

    fix: Globalstyle css is wrong

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import './assets/css/reset.css'
-import './assets/css/GlobalStyle.js'
+import './assets/css/globalstyle.css'
 import App from './App';
 
 ReactDOM.render(<App />, document.querySelector('.root'));
