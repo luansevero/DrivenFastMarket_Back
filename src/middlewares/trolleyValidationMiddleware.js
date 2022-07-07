8f7f1f0cec7cf578b07a845391ddafb58b5f574b
Luan Severo <luansevero@icloud.com>
2022-07-07 20:06:37 -0300

    refactor: searchProduct before enter in controller

--- src/middlewares/trolleyValidationMiddleware.js
+++ src/middlewares/trolleyValidationMiddleware.js
@@ -1,18 +1,15 @@
 import { db } from '../setup/db.js';
 
-async function isProductAlreadyInCostumerTrolley(req,res,next){
+async function searchProduct(req,res,next){
     const { costumer } = res.locals;
     const product = req.body;
     try{
-        const isProductAlreadyInCostumerTrolley = await db.collection("trolley-products").findOne({ userId: costumer._id, nome: product.nome });
-        if (isProductAlreadyInCostumerTrolley) {
-            return res.status(409).send("Esse produto já esta no carrinho!");
-        }
+        const trolleyProduct = await db.collection("trolley-products").findOne({ userId: costumer._id, nome: product.nome });
+        res.locals.trolleyProduct = trolleyProduct;
         next()
     }catch(error){
         console.log("[Error] - isProductAlreadyInCostumerTrolley Middleware")
     }
-    
 }
 
 export default isProductAlreadyInCostumerTrolley;
