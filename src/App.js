c50eef6226f3c4b273d34bd77f7df7c799fc5fe3
Luan Severo <luansevero@icloud.com>
2022-07-07 14:20:25 -0300

    feat: UserContext with back-end url

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 import TokenContext from "./contexts/TokenContext.js";
+import UserContext from "./contexts/UserContext.js";
 
 import HomePage from './components/pages/HomePage';
 import ProductCategoryPage from './components/pages/ProductCategoryPage';
@@ -15,22 +16,25 @@ import RegisterPage from './components/pages/RegisterPage';
 
 export default function App() {
     const [token, setToken] = useState(null);
+    const [url, setUrl] = useState('https://driven-fast-market.herokuapp.com/');
 
     return (
         <TokenContext.Provider value={{setToken, token}}>
-            <BrowserRouter>
-                <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/:categoriaProduto" element={<ProductCategoryPage />} />
-                        <Route path="/:categoriaProduto/:tipoProduto" element={<ProductTypePage />} />
-                        <Route path="/carrinho" element={<TrolleyPage />} />
-                        <Route path="/pagamento" element={<PaymentPage />} />
-                        <Route path="/pedidos" element={<RequestPage />} />
-                        <Route path="/menu" element={<MenuPage />} />
-                        <Route path="/menu/login" element={<LoginPage />} />
-                        <Route path="/menu/registro" element={<RegisterPage />} />
-                </Routes>
-            </BrowserRouter>
+            <UserContext.Provider value={{ url }}>
+                <BrowserRouter>
+                    <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/:categoriaProduto" element={<ProductCategoryPage />} />
+                            <Route path="/:categoriaProduto/:tipoProduto" element={<ProductTypePage />} />
+                            <Route path="/carrinho" element={<TrolleyPage />} />
+                            <Route path="/pagamento" element={<PaymentPage />} />
+                            <Route path="/pedidos" element={<RequestPage />} />
+                            <Route path="/menu" element={<MenuPage />} />
+                            <Route path="/menu/login" element={<LoginPage />} />
+                            <Route path="/menu/registro" element={<RegisterPage />} />
+                    </Routes>
+                </BrowserRouter>
+            </UserContext.Provider>
         </TokenContext.Provider>
     )
 }
