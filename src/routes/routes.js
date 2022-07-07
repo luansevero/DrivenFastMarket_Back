485937d072ded1668c759f8bd9c0bc16bd1f9514
Luan Severo <luansevero@icloud.com>
2022-07-06 21:30:45 -0300

    feat: trolleyRoute connect to the main route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRoute from "./authRoute.js";
+import trolleyRoute from "./trolleyRoute.js";
 
 const router = Router();
 
 router.use(authRoute);
+router.use(trolleyRoute);
 
 export default router;
