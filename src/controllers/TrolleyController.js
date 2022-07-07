dd3b19370739c6ed96a43b00855977d2b9319ab3
Luan Severo <luansevero@icloud.com>
2022-07-06 21:55:24 -0300

    test: trolleyroute(POST -> working clen)

--- src/controllers/TrolleyController.js
+++ src/controllers/TrolleyController.js
@@ -6,7 +6,7 @@ const trolley = {
         const { costumer } = res.locals;
         const product = req.body;
         try{
-            await db.collection('trolley-products').insertOne({...product, userId: costumer._id, productId: product._id})
+            await db.collection('trolley-products').insertOne({...product, userId: costumer._id, productId: `${product.categoria}.${product.tipo}`})
 
             res.sendStatus(201);
         }catch(error){
