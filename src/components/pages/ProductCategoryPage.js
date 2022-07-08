3b5425243868c6ac4773a24359e5a9ff19e72eee
Luan Severo <luansevero@icloud.com>
2022-07-08 15:14:04 -0300

    style: NavBar

--- src/components/pages/ProductCategoryPage.js
+++ src/components/pages/ProductCategoryPage.js
@@ -9,7 +9,14 @@ export default function ProductCategoryPage(){
                     <ion-icon name="search-outline"></ion-icon>
             </Header>
             <NavBar>
-                
+                <ul>
+                    <li className="info"><span>Frutas</span></li>
+                    <li className="info"><span>Ovos</span></li>
+                    <li className="info"><span>Legumes</span></li>
+                    <li className="info"><span>Vegetais</span></li>
+                    <li className="info"><span>Frutas</span></li>
+                    <li className="info"><span>Frutas</span></li>
+                </ul>
             </NavBar>
             <Main>
                 <Section>
@@ -20,7 +27,7 @@ export default function ProductCategoryPage(){
                     <ProductContainer>
                         <ProductBox>
                             <ProductImgBox>
-                                <div className="amount"><span>500g</span></div>
+                                <div className="info"><span>500g</span></div>
                                 <img src='https://frutasbrasilsul.com.br/wp-content/uploads/banana-nanica.png' alt='' />
                                 <button><h3>+</h3></button>
                             </ProductImgBox>
@@ -29,7 +36,7 @@ export default function ProductCategoryPage(){
                         </ProductBox>
                         <ProductBox>
                             <ProductImgBox>
-                                <div className="amount"><span>500g</span></div>
+                                <div className="info"><span>500g</span></div>
                                 <img src='https://frutasbrasilsul.com.br/wp-content/uploads/banana-nanica.png' alt='' />
                                 <button><h3>+</h3></button>
                             </ProductImgBox>
@@ -38,7 +45,7 @@ export default function ProductCategoryPage(){
                         </ProductBox>
                         <ProductBox>
                             <ProductImgBox>
-                                <div className="amount"><span>500g</span></div>
+                                <div className="info"><span>500g</span></div>
                                 <img src='https://frutasbrasilsul.com.br/wp-content/uploads/banana-nanica.png' alt='' />
                                 <button><h3>+</h3></button>
                             </ProductImgBox>
@@ -55,7 +62,7 @@ export default function ProductCategoryPage(){
                     <ProductContainer>
                         <ProductBox>
                             <ProductImgBox>
-                                <div className="amount"><span>500g</span></div>
+                                <div className="info"><span>500g</span></div>
                                 <img src='https://frutasbrasilsul.com.br/wp-content/uploads/banana-nanica.png' alt='' />
                                 <button><h3>+</h3></button>
                             </ProductImgBox>
@@ -64,7 +71,7 @@ export default function ProductCategoryPage(){
                         </ProductBox>
                         <ProductBox>
                             <ProductImgBox>
-                                <div className="amount"><span>500g</span></div>
+                                <div className="info"><span>500g</span></div>
                                 <img src='https://frutasbrasilsul.com.br/wp-content/uploads/banana-nanica.png' alt='' />
                                 <button><h3>+</h3></button>
                             </ProductImgBox>
@@ -73,7 +80,7 @@ export default function ProductCategoryPage(){
                         </ProductBox>
                         <ProductBox>
                             <ProductImgBox>
-                                <div className="amount"><span>500g</span></div>
+                                <div className="info"><span>500g</span></div>
                                 <img src='https://frutasbrasilsul.com.br/wp-content/uploads/banana-nanica.png' alt='' />
                                 <button><h3>+</h3></button>
                             </ProductImgBox>
@@ -82,7 +89,76 @@ export default function ProductCategoryPage(){
                         </ProductBox>
                     </ProductContainer>
                 </Section>
-
+                <Section>
+                    <TitleContainer>
+                        <span className="type">Frutas</span>
+                        <h3>VER MAIS</h3>
+                    </TitleContainer>
+                    <ProductContainer>
+                        <ProductBox>
+                            <ProductImgBox>
+                                <div className="info"><span>500g</span></div>
+                                <img src='https://frutasbrasilsul.com.br/wp-content/uploads/banana-nanica.png' alt='' />
+                                <button><h3>+</h3></button>
+                            </ProductImgBox>
+                            <h2>R$ 10,99</h2>
+                            <span>Laranja Pera Aprox</span>
+                        </ProductBox>
+                        <ProductBox>
+                            <ProductImgBox>
+                                <div className="info"><span>500g</span></div>
+                                <img src='https://frutasbrasilsul.com.br/wp-content/uploads/banana-nanica.png' alt='' />
+                                <button><h3>+</h3></button>
+                            </ProductImgBox>
+                            <h2>R$ 10,99</h2>
+                            <span>Laranja Pera Aprox</span>
+                        </ProductBox>
+                        <ProductBox>
+                            <ProductImgBox>
+                                <div className="info"><span>500g</span></div>
+                                <img src='https://frutasbrasilsul.com.br/wp-content/uploads/banana-nanica.png' alt='' />
+                                <button><h3>+</h3></button>
+                            </ProductImgBox>
+                            <h2>R$ 10,99</h2>
+                            <span>Laranja Pera Aprox</span>
+                        </ProductBox>
+                    </ProductContainer>
+                </Section>
+                <Section>
+                    <TitleContainer>
+                        <span className="type">Frutas</span>
+                        <h3>VER MAIS</h3>
+                    </TitleContainer>
+                    <ProductContainer>
+                        <ProductBox>
+                            <ProductImgBox>
+                                <div className="info"><span>500g</span></div>
+                                <img src='https://frutasbrasilsul.com.br/wp-content/uploads/banana-nanica.png' alt='' />
+                                <button><h3>+</h3></button>
+                            </ProductImgBox>
+                            <h2>R$ 10,99</h2>
+                            <span>Laranja Pera Aprox</span>
+                        </ProductBox>
+                        <ProductBox>
+                            <ProductImgBox>
+                                <div className="info"><span>500g</span></div>
+                                <img src='https://frutasbrasilsul.com.br/wp-content/uploads/banana-nanica.png' alt='' />
+                                <button><h3>+</h3></button>
+                            </ProductImgBox>
+                            <h2>R$ 10,99</h2>
+                            <span>Laranja Pera Aprox</span>
+                        </ProductBox>
+                        <ProductBox>
+                            <ProductImgBox>
+                                <div className="info"><span>500g</span></div>
+                                <img src='https://frutasbrasilsul.com.br/wp-content/uploads/banana-nanica.png' alt='' />
+                                <button><h3>+</h3></button>
+                            </ProductImgBox>
+                            <h2>R$ 10,99</h2>
+                            <span>Laranja Pera Aprox</span>
+                        </ProductBox>
+                    </ProductContainer>
+                </Section>
 
             </Main>
         </Body>
@@ -102,9 +178,23 @@ const Body =  styled.div`
         cursor: pointer;
         outline: inherit;
     }
+    .info{
+        display:flex;
+        align-items: center;
+        justify-content:center;
+        border: 1px solid #FF4791;
+        border-radius: 10px;
+        span{
+            font-weight:300;
+            font-size: 18px;
+            line-height: 18px;
+            color: #FF4791;
+        }
+    }
 
 `
 const Header = styled.header`
+    z-index: 1;
     position: fixed;
     top: 0;
     left: 0;
@@ -114,7 +204,7 @@ const Header = styled.header`
     align-items:center;
     justify-content:space-between;
     background-color: #FFFFFF;
-    padding: 10px;
+    padding: 5px 10px 0px 10px;
     ion-icon{
         width: 40px;
         height: 40px;
@@ -131,13 +221,30 @@ const Header = styled.header`
 }
 `
 const NavBar = styled.nav`
+    z-index: 1;
     position: fixed;
     top: 70px;
     left: 0px;
     width: 100%;
-    height: 50px;
-    padding: 15px;
-    background-color:#FFFFFF;
+    overflow-y: hidden;
+    padding: 0 15px;
+    background-color: #ffffff;
+    ul{
+        padding: 0 15px;
+        display:flex;
+        li:first-of-type{
+            margin-left:0;
+        }
+        li:last-of-type{
+            margin-right:0;
+        }
+    }
+    li{
+        background-color: #ffffff;
+        padding: 10px;
+        margin: 0 5px;
+        height: 40px;
+    }
 `
 const Main =  styled.main`
     margin-top: 120px;
@@ -206,16 +313,11 @@ const ProductImgBox = styled.div`
         width:105px;
         height:105px;
     }
-    div.amount{
+    div.info{
         position: absolute;
         top: 7.5px;
         left: 87.5px;
-        display:flex;
-        align-items:center;
-        justifty-content:center;
         padding: 0px 5px;
-        border: 1px solid #FF4791;
-        background-color: #FFFFFF;
         border-radius: 12.5px;
         height: 25px;
         span{
@@ -224,7 +326,6 @@ const ProductImgBox = styled.div`
             font-weight: 400;
             font-size: 15px;
             line-height: 15px;
-            color: #FF4791;
         }
     }
     button{
