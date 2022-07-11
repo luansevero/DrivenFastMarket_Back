ff7200851f7519fca9baabd754f0da90a5e72f85
MariaEmiliaCastro <emyly.mary@gmail.com>
2022-07-11 17:52:14 -0300

    [FIX] Adicao de validacao de CreditCard do joi

--- src/schemas/checkoutSchema.js
+++ src/schemas/checkoutSchema.js
@@ -2,9 +2,9 @@ import joi from "joi";
 
 const paymentSchema = joi.object({
     cardName: joi.string().min(1).trim().required(),
-    cardNumber: joi.string().min(16).max(20).trim().required(),
-    cardSecurityCode: joi.string().min(3).max(3).trim().required(),
-    cardValidade: joi.string().min(5).trim().required()
+    cardNumber: joi.string().creditCard().trim().required(),
+    cardValidade: joi.string().min(5).max(5).trim().required(),
+    cardSecurityCode: joi.number().max(999).required()
 });
 
 export { paymentSchema };
